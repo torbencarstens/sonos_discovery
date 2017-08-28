174205c81caf5ccc0fa11c37c77ad3f9965680df
chabare <chabare95@gmail.com>
2017-08-28 19:11:37 +0200

    Let `start` take a reference

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ ST: urn:schemas-upnp-org:device:ZonePlayer:1"#.as_bytes();
         let _ = self.socket.sendto(player_search, 0, &self.multicast);
     }
 
-    pub fn start(self, timeout: Option<u32>, device_count: Option<usize>) -> HashSet<IpAddr> {
+    pub fn start(&self, timeout: Option<u32>, device_count: Option<usize>) -> HashSet<IpAddr> {
         let timeout = match timeout {
             Some(value) => { value }
             None => 5
