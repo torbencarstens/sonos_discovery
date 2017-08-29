7fae10b266bc620a04a03dc8ffb5fd71170e13b3
chabare <chabare95@gmail.com>
2017-08-29 19:08:44 +0200

    Add elapsed time to binary

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate sonos_discovery;
 
 use sonos_discovery::Discover;
+use std::time::Instant;
 
 fn main() {
+    let start_time = Instant::now();
+
     let discovery = Discover::new().unwrap();
-    let devices = discovery.start(None, Some(3)).unwrap();
-    for device in devices {
-        println!("{:?}", device)
+    let ips = discovery.start(None, Some(3)).unwrap();
+    for ip in ips {
+        println!("{:?}", ip)
     }
+
+    println!("\nTime: {:?}", start_time.elapsed())
 }
