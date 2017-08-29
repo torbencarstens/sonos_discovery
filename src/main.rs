6cd2d00eccb311ba558e9e1ea9a5a2d2819ae46c
chabare <chabare95@gmail.com>
2017-08-29 17:00:09 +0200

    [main] Unwrap discovery result

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate sonos_discovery;
 use sonos_discovery::Discover;
 
 fn main() {
-    let discovery = Discover::new();
+    let discovery = Discover::new().unwrap();
     let devices = discovery.start(None, None);
     for device in devices {
         println!("{}", device)
